a9de728d4df29b650f82aa080285dccf4b624894
TryaNever <alexispernette@outlook.fr>
2024-09-24 11:20:12 +0200

    création couleur,reset grille et colorier fini

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,17 +1,46 @@
-const selectAllDiv = document.querySelectorAll('.case-color')
+let selectAllDiv = ""
 const containColorSwitch = document.querySelector('.contain-color')
 const formNewColor = document.querySelector('form')
 const pAddColor = document.querySelector('.color-add')
 const inputColor = document.querySelector('input[type="color"]')
+const resetGrilleButton = document.querySelector('.fa-arrows-rotate')
 let colorSwitch = document.querySelectorAll('.color-choose')
-
+const containGrille = document.querySelector('.contain-grille')
 
 
 
 function Pyssla() {
     this.colorSelect = "blue";
     this.arrColorSpawn = ["black", "white", "purple", "green", "blue", "yellow", "orange"]
+    this.arrColorNameSpawn = ["black", "white", "purple", "green", "blue", "yellow", "orange"]
+    this.nameColor = "";
+    this.rowGrille = 20
+    this.columnGrille = 20
 
+
+    this.createGrille = function () {
+        if (selectAllDiv != "") {
+            selectAllDiv.forEach(el => {
+                el.remove()
+                console.log('test');
+                
+            });
+        }
+        for (let i = 0; i < this.rowGrille; i++) {
+            for (let u = 0; u < this.columnGrille; u++) {
+                let pysslaCase = document.createElement('div')
+                pysslaCase.classList.add('case-color')
+                containGrille.append(pysslaCase)
+            }
+        }
+        selectAllDiv = document.querySelectorAll('.case-color')
+        for (let i = 0; i < selectAllDiv.length; i++) {
+            selectAllDiv[i].addEventListener('click', function () {
+                grille.changeColor(selectAllDiv[i])
+            })
+        
+        }
+    }
 
     this.changeColor = function (div) {
         div.style.backgroundColor = this.colorSelect
@@ -21,21 +50,21 @@ function Pyssla() {
         colorSwitch.forEach(el => {
             containColorSwitch.removeChild(el)
         });
-        
+
         for (let i = 0; i < this.arrColorSpawn.length; i++) {
             let pNewColor = document.createElement('p')
             pNewColor.classList.add('color-choose')
+            pNewColor.textContent = this.arrColorNameSpawn[i]
             pNewColor.style.backgroundColor = this.arrColorSpawn[i]
             containColorSwitch.prepend(pNewColor)
         }
         colorSwitch = document.querySelectorAll('.color-choose')
         colorSwitch.forEach(el => {
-            el.addEventListener('click', function() {
+            el.addEventListener('click', function () {
                 grille.colorSelect = el.style.backgroundColor
-                console.log(grille.colorSelect);
             })
         });
-        
+
     }
     this.newColor = function () {
         this.arrColorSpawn.push(inputColor.value)
@@ -46,18 +75,13 @@ function Pyssla() {
 const grille = new Pyssla()
 
 
-for (let i = 0; i < selectAllDiv.length; i++) {
-    selectAllDiv[i].addEventListener('click', function () {
-        grille.changeColor(selectAllDiv[i])
-    })
-
-}
 
 pAddColor.addEventListener('click', function () {
     formNewColor.classList.remove('none')
+    grille.arrColorNameSpawn.push(grille.nameColor = window.prompt("Veuillez saisir le nom de la couleur souhaiter", ""))
 })
 
-inputColor.addEventListener('change', function(e) {
+inputColor.addEventListener('change', function (e) {
     e.preventDefault()
     grille.newColor()
     formNewColor.classList.add('none')
@@ -68,9 +92,17 @@ inputColor.addEventListener('change', function(e) {
 grille.createChangeColor()
 
 colorSwitch.forEach(el => {
-    el.addEventListener('click', function() {
+    el.addEventListener('click', function () {
         grille.colorSelect = el.style.backgroundColor
-        console.log(grille.colorSelect);
     })
 });
-console.log("test");
+
+
+resetGrilleButton.addEventListener('click', function () {
+    grille.createGrille()
+})
+
+
+grille.rowGrille = prompt('nombre de ligne -_-')
+grille.columnGrille = prompt('nombre de colone -_-')
+grille.createGrille()
